4b5de5ad48722bd456cf8e7930bb3810ec8383f0
Lucas-And3rson <lucasestuda09@gmail.com>
2023-05-04 15:34:13 -0300

    produto control update

---
+++ control/Produto.php
@@ -0,0 +1,16 @@
+<?php
+/* fluxograma?
+1 instanciar objeto
+2 "setar" informações vindas do formulário no objeto
+3 pegar um Dao
+4 salvar usando método salvar do DAO
+*/
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/aulaphp/bolateria/model/vo/Produto.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/aulaphp/bolateria/model/dao/ProdutoDAO.php';
+$obj = new Produto();
+$obj->setNome($_POST['nome']);
+$obj->setPrecokg($_POST['precokg']);
+
+ProdutoDAO::getInstance()->insert($obj);
+?>
