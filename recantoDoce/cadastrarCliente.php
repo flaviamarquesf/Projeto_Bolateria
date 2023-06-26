037d5b222ea696fc1eade803e612826632f289b2
Lucas-And3rson <lucasestuda09@gmail.com>
2023-06-26 20:01:51 -0300

    cadastro e Login

--- recantoDoce/cadastrarCliente.php
+++ recantoDoce/cadastrarCliente.php
@@ -74,10 +74,10 @@ if(isset($_GET['id'])){
                                 <input type="password" id="senha" name="senha" placeholder="Senha" class="list-unstyled-item list-hours-item d-flex">
                                 </div>
                                 <input class="btn btn-primary btn-user btn-block" type="submit" name="enviar" id="enviar" value="Cadastrar">
-                                    <br>
+                                    <br><br><br>
                             <p class="address mb-5">
                                 <em>
-                                    <a href="cadastrarCliente.php"><strong>Ainda não é cadastrado?</strong></a>
+                                    <a href="login.php"><strong>Login</strong></a>
                                     <br />
                                     
                                 </em>
@@ -87,29 +87,6 @@ if(isset($_GET['id'])){
                                 <br />
                                 (87) 98146-8271
                             </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="page-section about-heading">
-            <div class="container">
-                <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="assets/img/about.jpg" alt="..." />
-                <div class="about-heading-content">
-                    <div class="row">
-                        <div class="col-xl-9 col-lg-10 mx-auto">
-                            <div class="bg-faded rounded p-5">
-                                <h2 class="section-heading mb-4">
-                                    <span class="section-heading-upper">Strong Coffee, Strong Roots</span>
-                                    <span class="section-heading-lower">About Our Cafe</span>
-                                </h2>
-                                <p>Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
-                                <p class="mb-0">
-                                    We guarantee that you will fall in
-                                    <em>lust</em>
-                                    with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.
-                                </p>
-                            </div>
                         </div>
                     </div>
                 </div>
